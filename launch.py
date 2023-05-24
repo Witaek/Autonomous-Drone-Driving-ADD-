93abed34f36b2ab0999ced94e3d2fa96f24d12a8
Witaek <amedee.romain@gmail.com>
2023-05-24 12:02:36 +0200

    barycenter and target square

--- launch.py
+++ launch.py
@@ -35,6 +35,8 @@ class UserVision:
 
             # Read the saved image and display it
             #image = cv2.imread(filename)
+            height, width, _ = result.shape
+            print( height, width)
             cv2.imshow("Image", result.astype(np.uint8))
             cv2.waitKey(1)  # Add a small delay to allow GUI to refresh
 
@@ -43,7 +45,7 @@ bebop = Bebop()
 
 # connect to the bebop
 success = bebop.connect(5)
-
+bebop.set_max_altitude(2)
 if success:
     # start up the video
     bebopVision = DroneVision(bebop, is_bebop=True)
@@ -51,13 +53,13 @@ if success:
     userVision = UserVision(bebopVision, bebop)
     bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
     success = bebopVision.open_video()
-    bebop.safe_takeoff(10)
+    bebop.pan_tilt_camera(-30,0)
+
 
     if success:
         print("Vision successfully started!")
 
-        bebop.smart_sleep(20)
-        bebop.safe_land(10)
+        bebop.smart_sleep(15)
         bebopVision.close_video()
 
     # disconnect nicely so we don't need a reboot
